81bc4e3052eb7a79bb8daee9d236e83c96e7f2fc
Tim Stott <ta.stott@googlemail.com>
2017-03-21 21:51:23 +0000

    Implemented uninstall.ps1

--- NugetAddLinkedFile/tools/uninstall.ps1
+++ NugetAddLinkedFile/tools/uninstall.ps1
@@ -1,27 +1,24 @@
 param($installPath, $toolsPath, $package, $project)
 
-function AddLinkedFile(){
+function RemoveLinkedFile(){
     param($project, $filepath)
 
     $filename = Split-Path $filepath -Leaf
 
-    "Adding $filename as linked file"
+    "Removing $filename as linked file"
 
     $item = $project.ProjectItems | Where-Object { $_.Name -eq $filename } | Select-Object -First 1
 
     if ($item -ne $null){
         $item.Remove();
     }
-
-    $item = $project.ProjectItems.AddFromFile($filepath)
-    $item.Properties.Item("CopyToOutputDirectory").Value = [int]2;
 }
 
-"Adding linked files from $toolsPath to $($project.Name)"
+"Removing linked files from $toolsPath from $($project.Name)"
 
 Get-ChildItem $toolsPath -Exclude install.ps1,uninstall.ps1,init.ps1 |
 ForEach-Object {
-    AddLinkedFile $project $_.FullName
+    RemoveLinkedFile $project $_.FullName
 }
 
 
