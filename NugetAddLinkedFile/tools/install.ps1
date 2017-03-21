8813e7e5e8792dafdb14d9bdc17cad09fcefde24
Tim Stott <ta.stott@googlemail.com>
2017-03-21 21:21:08 +0000

    Add linked file

--- NugetAddLinkedFile/tools/install.ps1
+++ NugetAddLinkedFile/tools/install.ps1
@@ -3,3 +3,6 @@ param($installPath, $toolsPath, $package, $project)
 $installPath
 $toolsPath
 $project
+
+$item = $project.ProjectItems.AddFromFile((Join-Path $toolsPath "test.exe"))
+$item.Properties.Item("CopyToOutputDirectory").Value = [int]2;
